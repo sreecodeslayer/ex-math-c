5001413c362a3551171c47164ee5291d77eec8eb
Sreenadh TC <kesav.tc8@gmail.com>
2020-01-28 23:03:37 +0530

    Use supervisor for genserver monitoring

--- lib/exmath.ex
+++ lib/exmath.ex
@@ -1,7 +1,34 @@
 defmodule Exmath do
+  use Application
+
   alias Exmath.Commands
 
-  def ping do
-    IO.puts(Commands.ping())
+  # Aliased functions for simplicity
+  def sin(angle) do
+    :ok = Commands.sin(angle)
+  end
+
+  def cos(angle) do
+    :ok = Commands.cos(angle)
+  end
+
+  def tan(angle) do
+    :ok = Commands.tan(angle)
+  end
+
+  def sqrt(number) do
+    :ok = Commands.sqrt(number)
+  end
+
+  # Application supervision tree
+  def start(_type, _args) do
+    children = [
+      %{
+        id: Commands,
+        start: {Commands, :connect, []}
+      }
+    ]
+
+    Supervisor.start_link(children, strategy: :one_for_one)
   end
 end
